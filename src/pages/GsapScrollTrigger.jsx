f7c1c8d71fe251ac983622f713bfa06aa07c47cb
Daniel Bauer <dan717@gmx.de>
2024-06-14 17:33:06 +0200

    learned about gsap.scrollTrigger

--- src/pages/GsapScrollTrigger.jsx
+++ src/pages/GsapScrollTrigger.jsx
@@ -1,68 +1,106 @@
+import { useGSAP } from '@gsap/react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/all'; // ScrollTrigger is a plugin that allows you to create animations that are triggered by the scroll position of the page.
+import { useRef } from 'react';
+
+// Then we have to register the ScrollTrigger plugin with gsap
+gsap.registerPlugin(ScrollTrigger);
+
 const GsapScrollTrigger = () => {
-  // TODO: Implement the gsap scroll trigger
+	// Then we have to define a reference for the ScrollTrigger. This reference will be used to create the ScrollTrigger instance.
+	const scrollRef = useRef();
 
-  return (
-    <main>
-      <h1>GsapScrollTrigger</h1>
+	// TODO: Implement the gsap scroll trigger
+	useGSAP(
+		() => {
+			// First we get access to the boxes. This is how we can get tthee children by using react ref
+			const boxes = gsap.utils.toArray(scrollRef.current.children);
 
-      <p className="mt-5 text-gray-500">
-        Gsap Scroll Trigger is a plugin that allows you to create animations
-        that are triggered by the scroll position of the page.
-      </p>
+			// Here we iterate over the array
+			boxes.forEach((box) => {
+				// we add an animation to each box element
+				gsap.to(box, {
+					x: 150 * (boxes.indexOf(box) + 5),
+					rotation: 360,
+					borderRadius: '100%',
+					scale: 1.5,
+					// scrollTrigger is the property where i define when the animation will start when it comes into view
+					scrollTrigger: {
+						trigger: box,
+						start: 'bottom bottom', // This means: start when the bottom of the box hits the bottom of the viewport
+						end: 'top 20%', // This means: end when the top of the box hits 20% of the viewport. The higher the percentage, the earlier the animation will end
+						scrub: true, // This makes the animattion smooth
+					},
+					ease: 'power1.inOut', //
+				});
+			});
+		},
+		{ scope: scrollRef }
+	);
 
-      <p className="mt-5 text-gray-500">
-        With ScrollTrigger, you can define various actions to be triggered at
-        specific scroll points, such as starting or ending an animation,
-        scrubbing through animations as the user scrolls, pinning elements to
-        the screen, and more.{" "}
-      </p>
+	return (
+		<main>
+			<h1>GsapScrollTrigger</h1>
 
-      <p className="mt-5 text-gray-500">
-        Read more about the{" "}
-        <a
-          href="https://gsap.com/docs/v3/Plugins/ScrollTrigger/"
-          target="_blank"
-          rel="noreferrer noopener nofollow"
-        >
-          gsap scroll trigger
-        </a>{" "}
-        method.
-      </p>
+			<p className='mt-5 text-gray-500'>
+				Gsap Scroll Trigger is a plugin that allows you to create
+				animations that are triggered by the scroll position of the
+				page.
+			</p>
 
-      <div className="w-full h-[70vh] flex justify-center items-center flex-col">
-        <p className="text-center text-gray-500">
-          Scroll down to see the animation
-        </p>
+			<p className='mt-5 text-gray-500'>
+				With ScrollTrigger, you can define various actions to be
+				triggered at specific scroll points, such as starting or ending
+				an animation, scrubbing through animations as the user scrolls,
+				pinning elements to the screen, and more.{' '}
+			</p>
 
-        <svg
-          className="animate-bounce mt-5"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="blue"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="M12 19V5" />
-          <path d="M5 12l7 7 7-7" />
-        </svg>
-      </div>
+			<p className='mt-5 text-gray-500'>
+				Read more about the{' '}
+				<a
+					href='https://gsap.com/docs/v3/Plugins/ScrollTrigger/'
+					target='_blank'
+					rel='noreferrer noopener nofollow'
+				>
+					gsap scroll trigger
+				</a>{' '}
+				method.
+			</p>
 
-      <div className="mt-20 w-full h-screen">
-        <div
-          id="scroll-pink"
-          className="scroll-box w-20 h-20 rounded-lg bg-pink-500"
-        />
-        <div
-          id="scroll-orange"
-          className="scroll-box w-20 h-20 rounded-lg bg-orange-500"
-        />
-      </div>
-    </main>
-  );
+			<div className='w-full h-[70vh] flex justify-center items-center flex-col'>
+				<p className='text-center text-gray-500'>
+					Scroll down to see the animation
+				</p>
+
+				<svg
+					className='animate-bounce mt-5'
+					xmlns='http://www.w3.org/2000/svg'
+					width='24'
+					height='24'
+					viewBox='0 0 24 24'
+					fill='none'
+					stroke='blue'
+					strokeWidth='2'
+					strokeLinecap='round'
+					strokeLinejoin='round'
+				>
+					<path d='M12 19V5' />
+					<path d='M5 12l7 7 7-7' />
+				</svg>
+			</div>
+			{/* Here we have to set the ref to target the containing div elementt in the DOM */}
+			<div className='mt-20 w-full h-screen' ref={scrollRef}>
+				<div
+					id='scroll-pink'
+					className='scroll-box w-20 h-20 rounded-lg bg-pink-500'
+				/>
+				<div
+					id='scroll-orange'
+					className='scroll-box w-20 h-20 rounded-lg bg-orange-500'
+				/>
+			</div>
+		</main>
+	);
 };
 
 export default GsapScrollTrigger;
